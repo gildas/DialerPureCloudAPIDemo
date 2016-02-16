1a9965c92c4cded0c850bd4f9d4cf72daad5ea3f
Gildas Cherruel <gildas.cherruel@inin.com>
2016-02-16 17:04:16 +0900

    Main page from previous Dialer API site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var current_user = null;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', current_user: current_user });
 });
 
 module.exports = router;
