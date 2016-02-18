8aad8a62fdb3d98f2b3f87fe9d1e8607b56ea559
Gildas Cherruel <gildas.cherruel@inin.com>
2016-02-18 19:58:27 +0900

    Export Contacts

--- routes/contactlists.js
+++ routes/contactlists.js
@@ -20,39 +20,45 @@ router.param('id', function(req, res, next, id) {
   next();
 });
 
-// GET /campaigns - index
+// GET /contactlists - index
 router.get('/', function(req, res, next) {
   console.log("Listing all %s objects", model);
   res.render('contactlists/index', { title: 'DialerPureCloudAPIDemo' });
 });
 
-// POST /campaigns - Create
+// POST /contactlists - Create
 router.post('/', function(req, res, next) {
   console.log("Creating %s %s", model);
   res.status(404).send("Not Implemented");
 });
 
-// GET /campaigns/:id - Read
+// GET /contactlists/:id - Read
 router.get('/:id', function(req, res, next) {
   console.log("Showing %s %s", model, req.id);
   res.render('contactlists/show', { title: 'DialerPureCloudAPIDemo', id: req.id });
 });
 
-// GET /campaigns/:id/edit - Edit
+// GET /contactlists/:id/edit - Edit
 router.get('/:id/edit', function(req, res, next) {
   console.log("Editing %s %s", model, req.id);
   res.render('contactlists/edit', { title: 'DialerPureCloudAPIDemo', id: req.id });
 });
 
-// PUT /campaigns/:id - Update
+// PUT /contactlists/:id - Update
 router.put('/:id', function(req, res, next) {
   console.log("Updating %s %s", model, req.id);
 });
 
-// DELETE /campaigns/:id - Destroy
+// DELETE /contactlists/:id - Destroy
 router.delete('/:id', function(req, res, next) {
   console.log("Deleting %s %s", model, req.id);
   res.status(404).send("Not Implemented");
+});
+
+// GET /contactlists/:id - Read
+router.get('/:id/contacts/export', function(req, res, next) {
+  console.log("Exporting Contacts for %s %s", model, req.id);
+  res.render('contactlists/export_contacts', { title: 'DialerPureCloudAPIDemo', id: req.id });
 });
 
 module.exports = router;
