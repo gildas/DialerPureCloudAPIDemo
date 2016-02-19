c4bd9e204721d0acff30c54d9b0896a15ef91564
Gildas Cherruel <gildas.cherruel@inin.com>
2016-02-19 19:54:01 +0900

    Add routes for contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,48 +9,63 @@ router.param('id', function(req, res, next, id) {
   next();
 });
 
-// GET /contacts - index
-router.get('/', function(req, res, next) {
-  console.log("Listing all %s objects", model);
-  res.render('contacts/index', { title: 'DialerPureCloudAPIDemo' });
-});
-
-// GET /contacts/new - new
-router.get('/new', function(req, res, next) {
-  console.log("Preparing new %s object", model);
+// grab contact list id
+function getContactListId(req)
+{
   if (req.query.contactlist_id == null)
   {
     console.error("Missing contact list id");
     res.status(400).send("Missing ContactList id");
+    //throw;
+    return null;
   }
-  res.render('contacts/new', { title: 'DialerPureCloudAPIDemo', contactlist_id: req.query.contactlist_id});
+  return req.query.contactlist_id;
+}
+
+// GET /contacts - index
+router.get('/', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
+  console.log("Listing all %s objects for ContactList %s", model, contactlist_id);
+  res.render('contacts/index', { title: 'DialerPureCloudAPIDemo', contactlist_id: contactlist_id });
+});
+
+// GET /contacts/new - new
+router.get('/new', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
+  console.log("Preparing new %s object in ContactList %s", model, contactlist_id);
+  res.render('contacts/new', { title: 'DialerPureCloudAPIDemo', contactlist_id: contactlist_id });
 });
 
 // POST /contacts - Create
 router.post('/', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
   console.log("Creating %s %s", model);
   res.status(404).send("Not Implemented");
 });
 
 // GET /contacts/:id - Read
 router.get('/:id', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
   console.log("Showing %s %s", model, req.id);
-  res.render('contacts/show', { title: 'DialerPureCloudAPIDemo', id: req.id });
+  res.render('contacts/show', { title: 'DialerPureCloudAPIDemo', id: req.id, contactlist_id: getContactListId(req) });
 });
 
 // GET /contacts/:id/edit - Edit
 router.get('/:id/edit', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
   console.log("Editing %s %s", model, req.id);
-  res.render('contacts/edit', { title: 'DialerPureCloudAPIDemo', id: req.id });
+  res.render('contacts/edit', { title: 'DialerPureCloudAPIDemo', id: req.id, contactlist_id: getContactListId(req) });
 });
 
 // PUT /contacts/:id - Update
 router.put('/:id', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
   console.log("Updating %s %s", model, req.id);
 });
 
 // DELETE /contacts/:id - Destroy
 router.delete('/:id', function(req, res, next) {
+  var contactlist_id = getContactListId(req);
   console.log("Deleting %s %s", model, req.id);
   res.status(404).send("Not Implemented");
 });
